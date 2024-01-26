212d3e10e8a52e948a19fed180dc459cb67aeabb
Jana Schmälzle <jana@familie-schmaelzle.de>
2024-01-26 16:27:54 +0100

    add FigletFont

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,13 @@
 package org.example;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        String asciiWord = FigletFont.convertOneLine("Hello World!");
+        System.out.println(asciiWord);
 	System.out.println("How are you today?");
     }
 }
